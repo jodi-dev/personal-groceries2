142dc7803938b399cef367c043563b19528ef8f4
Jodi <dqiao100@uottawa.ca>
2021-05-31 22:31:21 -0400

    updated readme, added clickable stretched link in cards

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # personal-groceries2
 Improved website of personal-groceries.
+
+## List of improvements
+
+- replaced horticultural dingbat with leaf emoji to indicate organics
+- added more emoji to liven up and provide visuals to help people recognize what to click
+
+
+## In-Progress
+
+- [stretched link over card](https://getbootstrap.com/docs/4.4/utilities/stretched-link/)
+
+Make card clickable as effort to make app more accessible to people with mobility issues. Goal: not needing to click small checkbox to add items to cart.
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,29 @@ $(document).ready(function(){
     if (allergiesChecked.length == 0) {
         all_items.forEach(p=> {
             content += 
-            `<div class="card">
-                <div class="card-body">
-                    <div class="form-check pull-right">
-                        <input class="form-check-input" type="checkbox" name="product" value=${p[0]}>
-                        <label class="form-check-label"></label>
+            `
+                <div class="card">
+                    <div class="card-body">
+                        <div class="form-check pull-right">
+                            <input class="form-check-input" type="checkbox" name="product" value=${p[0]}>
+                            <label class="form-check-label"></label>
+                        </div>
+                        <h4 class="card-title">${p[0]}</h4>
+                        <p class="card-text">$ ${p[1]}</p>
+                        <a onClick="test()" class="stretched-link"></a>
                     </div>
-                    <h4 class="card-title">${p[0]}</h4>
-                    <p class="card-text">$ ${p[1]}</p>
                 </div>
-            </div>`
+            `
         });
         document.getElementById("shop").innerHTML = content;
     }
     document.getElementById("min-price").value = minPrice;
     document.getElementById("max-price").value = maxPrice;
 });
+
+function test() {
+    alert("!");
+}
 
 var chosenProducts = [];
 
